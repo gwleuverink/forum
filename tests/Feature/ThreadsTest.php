c57362acd2becc5766ccdd0dbc2092e29740b6b1
Willem Leuverink <willem@leuver.ink>
2018-03-01 18:12:42 +0100

    unit & featuretested that a user can read replies and a reply belongs to a user

--- tests/Feature/ThreadsTest.php
+++ tests/Feature/ThreadsTest.php
@@ -9,21 +9,32 @@ class ThreadsTest extends TestCase
 {
     use DatabaseMigrations;
 
+    public function setUp()
+    {
+        parent::setUp();
+        $this->thread = factory('App\Thread')->create();
+    }
+
     /** @test */
     public function a_user_can_view_all_threads()
     {
-        $thread = factory('App\Thread')->create();
-
-        $response = $this->get('/threads');
-        $response->assertSee($thread->title);
+        $this->get('/threads')
+            ->assertSee($this->thread->title);
     }
 
     /** @test */
     public function a_user_can_read_a_single_thread()
     {
-        $thread = factory('App\Thread')->create();
+        $this->get('/thread/' . $this->thread->id)
+            ->assertSee($this->thread->title);
+    }
 
-        $response = $this->get('/thread/' . $thread->id);
-        $response->assertSee($thread->title);
+    /** @test */
+    public function a_user_can_read_replies_that_are_associated_with_a_thread()
+    {
+        $reply = factory('App\Reply')->create(['thread_id' => $this->thread->id]);
+
+        $this->get('/thread/' . $this->thread->id)
+            ->assertSee($reply->body);
     }
 }
